7a773cbf9bbb87f8957db3a81de7f8d563027f79
alibekshak <alibekshakiro1@gmail.com>
2023-12-01 09:45:58 +0600

    Viewer app

---
+++ Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viewer
+//
+//  Created by Apple on 01.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
