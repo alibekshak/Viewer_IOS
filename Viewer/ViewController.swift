27549cae596302ea2e5ba785e39b7422d73997aa
alibekshak <alibekshakiro1@gmail.com>
2023-12-01 10:29:21 +0600

    Creating UITableViewController. Checking what we have in a cell.

--- Viewer/ViewController.swift
+++ Viewer/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var image = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+                image.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return image.count
     }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Image", for: indexPath)
+        cell.textLabel?.text = image[indexPath.row]
+        return cell
+    }
 }
 
